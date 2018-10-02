425adab1e332637f9127caeeb497ae2f231b1945
RenMG <ren.m.goodman@gmail.com>
2018-10-01 17:36:15 -0700

    adding new files, testing git

---
+++ BASICS-For_While_Loop.py
@@ -0,0 +1,16 @@
+#going over basics:
+'''13. Write a short program that prints the numbers 1 to 10 using a for loop.
+Then write an equivalent program that prints the numbers 1 to 10 using
+a while loop'''
+
+#also testing git commit shit
+
+#for loop:
+for x in range(0,11,1):
+    print ('for loop: {0}').format(x)
+
+#while loop:
+count = 0
+while count < 11:
+    print('while loop: {0}').format(count)
+    count = count+1
