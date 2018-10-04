ac67df8bbdd693ab034d47b9a642c40370ae3009
RenMG <ren.m.goodman@gmail.com>
2018-10-03 22:32:13 -0700

    ABS-CommaCode.py, went in made it complicated instead o thinking how to really manipulate lists and strings...the power of join

---
+++ ABS-CommaCode.py
@@ -0,0 +1,16 @@
+# Comma Code
+# Day 3 of 100
+
+list_info = ['apples', 'bananas', 'tofu', 'cats']
+
+out = ', '.join(list_info[:-1])
+end = list_info[-1]
+
+print(out + ',and ' + end)
+
+'''
+Lesson learned:
+
+Keep it simple.  My first attempts at this i went into loops, which got ugly, instead of really thinking about it from an extrapolation.
+
+'''
