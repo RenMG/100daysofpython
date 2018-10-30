6f66bf8b7e4958b675555c0fbfe3681f8315aa46
RenMG <ren.goodman@crowdstrike.com>
2018-10-29 21:04:53 -0700

    Added best practice

--- CodeWars-LongestOfTwo.py
+++ CodeWars-LongestOfTwo.py
@@ -5,7 +5,6 @@ containing distinct letters, each taken only once - coming from s1 or s2.
 Examples: ``` a = "xyaabbbccccdefww" b = "xxxxyyyyabklmopq" longest(a, b) -> "abcdefklmopqwxy" 
 a = "abcdefghijklmnopqrstuvwxyz" longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" ```
 '''
-
 def longest(s1, s2):
     if(s1.isalpha == False):
         raise Exception('{} is not a string, please make sure you enter a string.'.format(s1))
@@ -27,5 +26,11 @@ def longest(s1, s2):
         official_list = ''.join(new_list)
         print(official_list)
         return(official_list)
+
+"""
+This is the consolodated solution.  set creates a unique set between the 2 strings and then sorted wraps around the created string.  done.
+def longest(a1, a2):
+    print("".join(sorted(set(a1 + a2))))
+    return "".join(sorted(set(a1 + a2)))"""
 
 longest('xyaabbbccccdefww', 'xxxxyyyyabklmopq')
